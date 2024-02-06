30c94871db6a4bb6dac5ef7e710853daf7aec080
neoarmmon <neo.armmon@educa.jcyl.es>
2024-02-06 10:59:01 +0100

    Usuarios
    Usuarios y roles usuarios

--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 //Tablas E/R
 Genero(Nombre, Descripcion)
-Juegos(Titulo, -Generos, Descripcion, Votos Positivos, Votos Negativos, Enlace a ranking)
-Usuario (Usuario, Contraseña, Rol)
 Roles_Usuario(Nombre, Descripcion)
+Juegos(Titulo, -Generos, Descripcion, Votos Positivos, Votos Negativos)
+Usuario (Usuario, Contraseña, -Rol)
---
+++ videjueguitos/api_arcade/src/Entity/RolesUsuario.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RolesUsuarioRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RolesUsuarioRepository::class)]
+class RolesUsuario
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $descripcion = null;
+
+    #[ORM\OneToMany(targetEntity: Usuario::class, mappedBy: 'rol', orphanRemoval: true)]
+    private Collection $usuarios;
+
+    public function __construct()
+    {
+        $this->usuarios = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(?string $descripcion): static
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Usuario>
+     */
+    public function getUsuarios(): Collection
+    {
+        return $this->usuarios;
+    }
+
+    public function addUsuario(Usuario $usuario): static
+    {
+        if (!$this->usuarios->contains($usuario)) {
+            $this->usuarios->add($usuario);
+            $usuario->setRol($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsuario(Usuario $usuario): static
+    {
+        if ($this->usuarios->removeElement($usuario)) {
+            // set the owning side to null (unless already changed)
+            if ($usuario->getRol() === $this) {
+                $usuario->setRol(null);
+            }
+        }
+
+        return $this;
+    }
+}
