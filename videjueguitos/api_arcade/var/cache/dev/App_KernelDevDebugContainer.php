1ac771bf7564d64c5fd4dc133c95a210f525a881
neoarmmon <neo.armmon@educa.jcyl.es>
2024-02-07 13:25:38 +0100

    BD update
    Se han agregado los generos y el usuario Juan

--- videjueguitos/api_arcade/var/cache/dev/App_KernelDevDebugContainer.php
+++ videjueguitos/api_arcade/var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerK81Yx95\App_KernelDevDebugContainer([
     'container.build_hash' => 'K81Yx95',
-    'container.build_id' => '7653ad75',
-    'container.build_time' => 1707308160,
+    'container.build_id' => 'b039555b',
+    'container.build_time' => 1707308316,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerK81Yx95');
