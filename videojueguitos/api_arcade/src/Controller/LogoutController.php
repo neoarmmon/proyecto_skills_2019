1e6809988c974e0d35a7789d5015f8cd693d922a
juanes84vdb <mariovj222003.mv2@gmail.com>
2024-02-16 13:58:14 +0100

    logout (funciona) registro(no funciona)

--- videojueguitos/api_arcade/src/Controller/LogoutController.php
+++ videojueguitos/api_arcade/src/Controller/LogoutController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ProfileController extends AbstractController
+class LogoutController extends AbstractController
 {
-    #[Route('/profile', name: 'app_user_profile')]
-    public function __invoke(): Response
+    #[Route('/logout', name: 'app_logout', methods:['GET'])]
+    public function logout()
     {
-        return $this->render('profile/index.html.twig');
+       throw new \Exception('Don\'t forget to activate');
     }
 }
