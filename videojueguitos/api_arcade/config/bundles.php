05329a9895c1c7de78d5811515691ffe624423d1
juanes84vdb <mariovj222003.mv2@gmail.com>
2024-02-22 18:48:53 +0100

    prueba token composer update funciona (extension php.ini sodium)

--- videojueguitos/api_arcade/config/bundles.php
+++ videojueguitos/api_arcade/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
