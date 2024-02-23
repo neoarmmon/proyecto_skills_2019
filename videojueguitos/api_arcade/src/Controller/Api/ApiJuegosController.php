125801815a1f2a7cac3f377241b9e3a67ee070ec
juanes84vdb <mariovj222003.mv2@gmail.com>
2024-02-23 08:54:41 +0100

    sin login

--- videojueguitos/api_arcade/src/Controller/Api/ApiJuegosController.php
+++ videojueguitos/api_arcade/src/Controller/Api/ApiJuegosController.php
@@ -16,68 +16,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\DBAL\Types\Types;
 
 
-#[Route('main/juegos')]
+#[Route('api/juegos')]
 class ApiJuegosController extends AbstractController
 {
-    #[Route('/todos', name: 'app_juegos_todos', methods: ['GET'])]
-    public function todosj(JuegosRepository $juegosRepository, Request $request): Response
-    {
-        $juegos=$juegosRepository->findAll(1);
-        $juegosArray=[];
-        foreach($juegos as $juego){
-            $generosArray = [];
-        foreach ($juego->getGeneros() as $genero) {
-            $generosArray[] = [
-                'nombre' => $genero->getNombre(), 
-            ];
-        }
-            $juegosArray[]=[
-                'nombre' => $juego->getNombre(),
-                'descripcion' => $juego->getDescripcion(),
-                'positivos' => $juego->getVotosPositivos(),
-                'negativos' => $juego->getVotosNegativos(),
-                'imagen' => base64_encode(stream_get_contents($juego->getImagen())),
-                'generos' => $generosArray
-            ];
-        }
-        $response = new JsonResponse();
-        $response->setData(
-            $juegosArray
-        );
-        return $response;
-    }
-    
-    #[Route('/genero{genero}', name: 'app_juegos_genero', methods: ['GET'])]
-    public function jugosg(JuegosRepository $juegosRepository,
-     Request $request,
-     JuegosGeneroRepository $juegosGeneroRepository,
-      int $genero): Response
-    {
-        $id_juegos = $juegosGeneroRepository->findByGeneroid($genero);
-        $juegosArray=[];
-        foreach($id_juegos as $juegos){
-            $juego=$juegosRepository->findByid($juegos->getIdJuego());
-            foreach ($juego as $game){
-                $generosArray = [];
-                foreach ($game->getGeneros() as $genero) {
-                    $generosArray[] = [
-                        'nombre' => $genero->getNombre(), 
-                    ];
-                }
-                $juegosArray[]=[
-                    'nombre' => $game->getNombre(),
-                    'descripcion' => $game->getDescripcion(),
-                    'positivos' => $game->getVotosPositivos(),
-                    'negativos' => $game->getVotosNegativos(),
-                    'imagen' => base64_encode(stream_get_contents($game->getImagen())),
-                    'generos' => $generosArray
-                ];
-            }
-        }
-        $response = new JsonResponse();
-        $response->setData(
-            $juegosArray
-        );
-        return $response;
-    }
 }
