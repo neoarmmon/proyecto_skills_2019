bd123be01aab1f9ffdbe86c3b11bd44ea51c95a0
juanes84vdb <mariovj222003.mv2@gmail.com>
2024-02-24 13:34:08 +0100

    si

--- videojueguitos/app_arcade_modulos/src/app/get-nombre.service.ts
+++ videojueguitos/app_arcade_modulos/src/app/get-nombre.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetNombreService {
+  token: string="";
 
-  private apiUrl = 'https://127.0.0.1:8000';
+    headers = new HttpHeaders({
+    'Authorization': `Bearer ${this.token}`
+  });
+
+  private apiUrl = 'https://127.0.0.1:8000/api/perfil';
 
   constructor(private http: HttpClient) {}
 
   // Función para obtener el nombre de usuario
   getUsername(token: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/get-username`, { token });
+    return this.http.post<any>(`${this.apiUrl}`, { token });
   }
 }
